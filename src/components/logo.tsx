1df04575f19efd7826e17263eb24de1890c389e9
WitekG14 <witold@grzegorczyk.it>
2026-02-18 13:56:03 +0000

    Logo (NextJS Image Component)

---
+++ src/components/logo.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+function Logo() {
+    return (
+        <div>
+            <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="EVENTO logo" width={53} height={12} />
+        </div>
+    )
+}
+
+export default Logo
