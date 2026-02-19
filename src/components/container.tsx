39b66de7216d470b9f94e534707ccb06ba9ccea9
WitekG14 <witold@grzegorczyk.it>
2026-02-19 14:02:04 +0000

    Finish Home Page Styling With Tailwind CSS

---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+function Container({ children }) {
+  return (
+    <div className="flex flex-col max-w-5xl min-h-screen mx-auto bg-white/[2%]">
+      {children}
+    </div>
+  );
+}
+
+export default Container;
