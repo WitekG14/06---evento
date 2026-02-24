51dc914f354ee459d6e80ab455c37161fa17a269
WitekG14 <witold@grzegorczyk.it>
2026-02-24 13:47:37 +0000

    Event Page Params (Get Event Slug From URL)

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,9 +1,11 @@
-function EventPage() {
-    return (
-        <main>
-            Event Page
-        </main>
-    )
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+async function EventPage({ params }: EventPageProps) {
+  return <main>Event Page</main>;
 }
 
-export default EventPage
+export default EventPage;
