f787e00f818863e232f833efc46592002706b9c9
WitekG14 <witold@grzegorczyk.it>
2026-02-24 13:58:38 +0000

    Image Component Advanced (+ Design Tips)

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -15,10 +15,26 @@ async function EventPage({ params }: EventPageProps) {
 
   return (
     <main>
-      <section>
-        <Image />
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          src={event.imageUrl}
+          className="object-cover z-0 blur-3xl"
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+
+        <div className="z-1 relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
       </section>
-      <div></div>
     </main>
   );
 }
