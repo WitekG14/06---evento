17d5f5bbd8fd75716800b3daea37a44ed96c72e4
WitekG14 <witold@grzegorczyk.it>
2026-02-24 13:42:23 +0000

    Fetch Events For Correct City

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,13 +9,12 @@ type EventsPageProps = {
 };
 
 async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
   const res = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${params.city}`,
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
   );
   const events: EventoEvent[] = await res.json();
   console.log(events);
-
-  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
