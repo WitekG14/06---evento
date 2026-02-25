9efef358a1552ce4836542b1d39e4fff61e2baa7
WitekG14 <witold@grzegorczyk.it>
2026-02-25 13:34:34 +0000

    Reusable Hover Effect (Tailwind @Apply Rule)

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -12,7 +12,7 @@ function EventCard({ event }: EventCardProps) {
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
     >
-      <section className="w-full h-full flex flex-col bg-white/[3%] relative rounded-xl overflow-hidden transition hover:scale-105 active:scale-[1.02]">
+      <section className="w-full h-full flex flex-col bg-white/[3%] relative rounded-xl overflow-hidden state-effects">
         <Image
           src={event.imageUrl}
           alt={event.name}
