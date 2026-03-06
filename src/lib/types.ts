bfbf22833cae3014754c4b01fcd10fcbfef95ef7
WitekG14 <witold@grzegorczyk.it>
2026-03-06 09:21:34 +0000

    Setup SQLite With Prisma

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,1 @@
-export type EventoEvent = {
-  id: number;
-  name: string;
-  city: string;
-  slug: string;
-  location: string;
-  date: Date;
-  organizerName: string;
-  imageUrl: string;
-  description: string;
-};
+export type { EventoEvent } from "@prisma/client";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { EventoEvent } from "./types";
+import { EventoEvent } from "@/generated/prisma";
 
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -14,7 +14,7 @@ export async function getEvent(slug: string) {
   const res = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
   );
-  const event = await res.json();
+  const event: EventoEvent[] = await res.json();
 
   return event;
 }
